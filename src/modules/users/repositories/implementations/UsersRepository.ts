e6b8d2b49ef75b2edf6d24ee3cee4c4207c28373
João Bispo <joaobispo2077@gmail.com>
2021-09-19 12:57:27 -0300

    feat: add methods into UsersRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,27 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {
+      relations: ['games'],
+    });
+
+    return user as User;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM users ORDER BY first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const findUsersByFullNameQuery = `SELECT * FROM users WHERE LOWER(first_name) = LOWER('${first_name}') AND LOWER(last_name) = LOWER('${last_name}')`;
+    console.log("query is",findUsersByFullNameQuery);
+
+    const users = await this.repository.query(findUsersByFullNameQuery); // Complete usando raw query
+
+    console.log("users are", users);
+    return users;
   }
 }
